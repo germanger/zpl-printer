87b381456e3254fb1ff1587dc65580269361c0a2
germanger <givicencio@gmail.com>
2017-06-06 17:13:04 -0400

    First version

---
+++ ZPLPrinterSolution/ZPLPrinterProject/ZPLPrinterForm.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace ZPLPrinterProject
+{
+    partial class ZPLPrinterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sourceTextBox = new System.Windows.Forms.TextBox();
+            this.previewButton = new System.Windows.Forms.Button();
+            this.widthTextbox = new System.Windows.Forms.TextBox();
+            this.heightTextbox = new System.Windows.Forms.TextBox();
+            this.unitsCombobox = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.printButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(928, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "File";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // sourceTextBox
+            // 
+            this.sourceTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sourceTextBox.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sourceTextBox.Location = new System.Drawing.Point(3, 3);
+            this.sourceTextBox.Multiline = true;
+            this.sourceTextBox.Name = "sourceTextBox";
+            this.sourceTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.sourceTextBox.Size = new System.Drawing.Size(458, 352);
+            this.sourceTextBox.TabIndex = 1;
+            // 
+            // previewButton
+            // 
+            this.previewButton.Location = new System.Drawing.Point(3, 3);
+            this.previewButton.Name = "previewButton";
+            this.previewButton.Size = new System.Drawing.Size(96, 34);
+            this.previewButton.TabIndex = 2;
+            this.previewButton.Text = "Preview with labery.com";
+            this.previewButton.UseVisualStyleBackColor = true;
+            this.previewButton.Click += new System.EventHandler(this.previewButton_Click);
+            // 
+            // widthTextbox
+            // 
+            this.widthTextbox.Location = new System.Drawing.Point(103, 3);
+            this.widthTextbox.Name = "widthTextbox";
+            this.widthTextbox.Size = new System.Drawing.Size(94, 20);
+            this.widthTextbox.TabIndex = 3;
+            // 
+            // heightTextbox
+            // 
+            this.heightTextbox.Location = new System.Drawing.Point(103, 29);
+            this.heightTextbox.Name = "heightTextbox";
+            this.heightTextbox.Size = new System.Drawing.Size(94, 20);
+            this.heightTextbox.TabIndex = 4;
+            // 
+            // unitsCombobox
+            // 
+            this.unitsCombobox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.unitsCombobox.FormattingEnabled = true;
+            this.unitsCombobox.Items.AddRange(new object[] {
+            "cm",
+            "inches"});
+            this.unitsCombobox.Location = new System.Drawing.Point(103, 55);
+            this.unitsCombobox.Name = "unitsCombobox";
+            this.unitsCombobox.Size = new System.Drawing.Size(94, 21);
+            this.unitsCombobox.TabIndex = 5;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.unitsCombobox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.heightTextbox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.widthTextbox, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 361);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 99);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Units";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Height";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width";
+            // 
+            // labelWebBrowser
+            // 
+            this.labelWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelWebBrowser.Location = new System.Drawing.Point(467, 3);
+            this.labelWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.labelWebBrowser.Name = "labelWebBrowser";
+            this.labelWebBrowser.Size = new System.Drawing.Size(458, 352);
+            this.labelWebBrowser.TabIndex = 7;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.labelWebBrowser, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.sourceTextBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.51095F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.48905F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(928, 548);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.previewButton);
+            this.flowLayoutPanel1.Controls.Add(this.printButton);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(467, 361);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(332, 99);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(105, 3);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(75, 34);
+            this.printButton.TabIndex = 3;
+            this.printButton.Text = "Print";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // ZPLPrinterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 572);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ZPLPrinterForm";
+            this.Text = "ZPL Printer and Viewer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ZPLPrinterForm_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TextBox sourceTextBox;
+        private System.Windows.Forms.Button previewButton;
+        private System.Windows.Forms.TextBox widthTextbox;
+        private System.Windows.Forms.TextBox heightTextbox;
+        private System.Windows.Forms.ComboBox unitsCombobox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser labelWebBrowser;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+    }
+}
+
---
+++ ZPLPrinterSolution/ZPLPrinterProject/ZPLPrinterForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using System.Net.Sockets;
+
+namespace ZPLPrinterProject
+{
+    public partial class ZPLPrinterForm : Form
+    {
+        private List<string> temporaryFiles = new List<string>();
+
+        public ZPLPrinterForm(string[] args)
+        {
+            InitializeComponent();
+
+            if (args.Length > 0)
+            {
+                OpenFile(args[0]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load app settings
+            var config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+
+            widthTextbox.Text = config.AppSettings.Settings["width"].Value;
+            heightTextbox.Text = config.AppSettings.Settings["height"].Value;
+            unitsCombobox.SelectedItem = config.AppSettings.Settings["units"].Value;
+        }
+
+        private void ZPLPrinterForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Delete temporary files
+            foreach (var filePath in temporaryFiles)
+            {
+                File.Delete(filePath);
+            }
+
+            // Save app settings
+            var config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+
+            config.AppSettings.Settings["width"].Value = widthTextbox.Text;
+            config.AppSettings.Settings["height"].Value = heightTextbox.Text;
+            config.AppSettings.Settings["units"].Value = unitsCombobox.SelectedItem.ToString();
+
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        public void OpenFile(string path)
+        {
+            sourceTextBox.Text = File.ReadAllText(path);
+            //previewButton_Click(null, null);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "zpl files (*.zpl)|*.zpl";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    OpenFile(openFileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void previewButton_Click(object sender, EventArgs e)
+        {
+            previewButton.Enabled = false;
+
+            // to inches
+            double width = Double.Parse(widthTextbox.Text);
+            double height = Double.Parse(heightTextbox.Text);
+
+            if (unitsCombobox.SelectedItem.Equals("cm"))
+            {
+                width = width * 0.393701;
+                height = height * 0.393701;
+            }
+
+            byte[] zplSourceBytes = Encoding.UTF8.GetBytes(sourceTextBox.Text);
+
+            var request = (HttpWebRequest)WebRequest.Create("http://api.labelary.com/v1/printers/8dpmm/labels/" + width + "x" + height + "/");
+            request.Method = "POST";
+            request.Accept = "application/pdf";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = zplSourceBytes.Length;
+
+            var requestStream = request.GetRequestStream();
+            requestStream.Write(zplSourceBytes, 0, zplSourceBytes.Length);
+            requestStream.Close();
+
+            // Temp file
+            string tempFileName = "temp_" + Guid.NewGuid().ToString() + ".pdf";
+
+            temporaryFiles.Add(tempFileName);
+
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseStream = response.GetResponseStream();
+                var fileStream = File.Create(tempFileName);
+                responseStream.CopyTo(fileStream);
+                responseStream.Close();
+                fileStream.Close();
+
+                previewButton.Enabled = true;
+
+                labelWebBrowser.Url = new Uri(String.Format("file:///{0}/" + tempFileName, Directory.GetCurrentDirectory()));
+            }
+            catch (WebException exception)
+            {
+                Console.WriteLine("Error: {0}", exception.Status);
+            }
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void printButton_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            printDialog.PrinterSettings = new PrinterSettings();
+
+            if (DialogResult.OK == printDialog.ShowDialog(this))
+            {
+                RawPrinterHelper.SendStringToPrinter(printDialog.PrinterSettings.PrinterName, sourceTextBox.Text);
+            }
+
+            /*Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.NoDelay = true;
+
+            IPAddress ip = IPAddress.Parse("192.168.1.22");
+            IPEndPoint ipep = new IPEndPoint(ip, 9100);
+            clientSocket.Connect(ipep);
+
+            byte[] fileBytes = Encoding.ASCII.GetBytes(sourceTextBox.Text);
+
+            clientSocket.Send(fileBytes);
+            clientSocket.Close();*/
+        }
+
+    }
+}
